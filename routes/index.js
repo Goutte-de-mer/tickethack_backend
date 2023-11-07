7ffa8df58d297565ce16a285fc4570c5b1ec2513
drajae <drajae2009@hotmail.com>
2023-11-07 14:39:59 +0100

    gcsgsv

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,19 @@
 var express = require('express');
 var router = express.Router();
+const Trip = require("../models/trips")
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+
+
+router.get('/trips', (req,res )=> {
+let departure = req.body.departure;
+let arrival = req.body.arrival;
+let date = req.body.date ;
+
+
+Trip.find({departure : departure, arrival : arrival  }).then(data =>{
+  res.json({data})
 });
+});
+
 
 module.exports = router;
