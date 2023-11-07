13ba705bbaa0ba4f5333871c14e538afcb158580
drajae <drajae2009@hotmail.com>
2023-11-07 17:08:46 +0100

    ticket

--- routes/bookings.js
+++ routes/bookings.js
@@ -2,15 +2,11 @@ var express = require('express');
 var router = express.Router();
 const Booking = require('../models/bookings');
 
-
 router.get('/' ,(req , res )=>{
     Booking.find().populate('trip')
     .then(data => { 
         res.json(data)
     })
-       
-})
-;
-
+});
 
 module.exports = router;
