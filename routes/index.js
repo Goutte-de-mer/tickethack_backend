4641aed6032767970a8743a7a69e9839f422c428
Marion <marionbailleux07@gmail.com>
2023-11-07 14:38:52 +0100

    tt

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,19 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
+const Trip = require("../models/trips");
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get("/trips/:departure/:arrival/:date", (req, res) => {
+  let departure = req.params.departure;
+  let arrival = req.params.arrival;
+  let date = req.params.date;
+
+  console.log(req.body);
+
+  Trip.find({ departure: departure, arrival: arrival, date: date }).then(
+    (data) => {
+      res.json(data);
+    }
+  );
 });
 
 module.exports = router;
